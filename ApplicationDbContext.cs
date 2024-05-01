16e295a9d07bf6c14c5844745ebf5bd3c5c91d4e
enova1 <xxclearwaterxx@gmail.com>
2024-05-01 16:38:05 -0400

    hangfire

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataAccess
+{
+    public class ApplicationDbContext44 : DbContext
+    {
+
+        /// <summary>
+        /// OnConfiguring method to configure the database connection.
+        /// </summary>
+        /// <param name="optionsBuilder"></param>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite($"Data Source=C:\\Users\\kozlo\\source\\GitHub\\DAL\\DataAccess\\bin\\Debug\\net8.0\\WebApi.db");
+            base.OnConfiguring(optionsBuilder);
+        }
+
+    }
+}
