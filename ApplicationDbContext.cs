4ed9f51919615470d0620c4030c447b97ec896ce
Tate2024 <chris.tate@b2gnow.com>
2024-05-14 14:38:57 -0400

    ds

--- ApplicationDbContext.cs
+++ ApplicationDbContext.cs
@@ -2,18 +2,11 @@
 
 namespace DataAccess
 {
-    public class ApplicationDbContext44 : DbContext
+    public class ApplicationDbContext : DbContext
     {
-
-        /// <summary>
-        /// OnConfiguring method to configure the database connection.
-        /// </summary>
-        /// <param name="optionsBuilder"></param>
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        /// <inheritdoc />
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
-            optionsBuilder.UseSqlite($"Data Source=C:\\Users\\kozlo\\source\\GitHub\\DAL\\DataAccess\\bin\\Debug\\net8.0\\WebApi.db");
-            base.OnConfiguring(optionsBuilder);
         }
-
     }
 }
