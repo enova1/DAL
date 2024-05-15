cbc838b73a237e41462a05c4d67f808aa94ce40b
Tate2024 <chris.tate@b2gnow.com>
2024-05-15 15:42:33 -0400

    t

--- AuthorizedUserDbContext.cs
+++ AuthorizedUserDbContext.cs
@@ -6,16 +6,10 @@ namespace DataAccess;
 public class AuthorizedUserDbContext : DbContext
 {
 
-    /// <summary>
-    /// OnConfiguring method to configure the database connection.
-    /// </summary>
-    /// <param name="optionsBuilder"></param>
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    /// <inheritdoc />
+    public AuthorizedUserDbContext(DbContextOptions<AuthorizedUserDbContext> options) : base(options)
     {
-        optionsBuilder.UseSqlite($"Data Source=C:\\Users\\kozlo\\source\\GitHub\\DAL\\DataAccess\\bin\\Debug\\net8.0\\WebApi.db");
-        base.OnConfiguring(optionsBuilder);
     }
-
     /// <summary>
     /// AuthUser table in the database.
     /// </summary>
