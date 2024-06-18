064ec9b4e15de39719a9e5cbdafd2b465af00f5f
Tate69 <chris.tate@b2gnow.com>
2024-06-18 12:36:05 -0400

    minor

--- BctModels/ContractType.cs
+++ BctModels/ContractType.cs
@@ -1,4 +1,7 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
+// ReSharper disable PropertyCanBeMadeInitOnly.Global
+// ReSharper disable EntityFramework.ModelValidation.UnlimitedStringLength
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
 
 namespace DataAccess.BctModels;
 
